31c2f193a4a12fe6da778b7158a9e6b6e60320bf
811khalid <khalidtewfiq151@gmail.com>
2024-03-27 04:26:03 -0700

    test

---
+++ 0x0A-configuration_management/install_flask.pp
@@ -0,0 +1,16 @@
+# Define a Puppet class for installing Flask version 2.1.0 using pip3
+class flask_install {
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+    require => Package['python3-pip'],
+  }
+}
+
+# Include the class to ensure Flask installation
+include flask_install
+
